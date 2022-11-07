69b77e3b5c213235fb829afdfa04d69a00d5d479
sjkhwaja <103473781+sjkhwaja@users.noreply.github.com>
2022-11-06 20:27:24 -0500

    Update testFile.py

--- testFile.py
+++ testFile.py
@@ -8,6 +8,13 @@ cwd = os.getcwd()
 # print cwd
 print(cwd)
 
+# create new dictionary
+covidData = pd.read_json('https://healthdata.gov/resource/8uup-3f6m.json')
+covidData
+
+# save to csv file
+covidData.to_csv('data/covidData.csv, index=None')
+
 # get the current time
 now = time.time()
 
@@ -15,5 +22,5 @@ now = time.time()
 start_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
 
 # create a new file in the current working directory
-with open(cwd + '/testFile_' + nowStr + '.txt', 'w') as f:
-    f.write(str(data))
+with open(cwd + '/home/saira/crontab/testFile_' + nowStr + '.txt', 'w') as f:
+    f.write(str(covidData))
